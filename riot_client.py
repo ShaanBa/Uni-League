0bd90f27ab271068d9d3ac172cca14192cc5ddd4
ShaanBa <162068352+ShaanBa@users.noreply.github.com>
2025-12-17 16:29:12 -0600

    moved loading env to top for efficiency

--- riot_client.py
+++ riot_client.py
@@ -3,10 +3,11 @@ import requests
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+
 
 
 def get_riot_account(game_name, tag_line):
-    load_dotenv()
     api_key = os.getenv("RIOT_API_KEY")
     r = requests.get(f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}", headers={"X-Riot-Token": api_key})
     return r.json()
