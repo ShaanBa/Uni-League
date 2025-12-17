aef6f0427eb019047942a26718775400088a66c6
ShaanBa <162068352+ShaanBa@users.noreply.github.com>
2025-12-17 16:27:04 -0600

    refactor: moved functionality into new get_riot_account function

--- riot_client.py
+++ riot_client.py
@@ -3,11 +3,10 @@ import requests
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
 
-api_key = os.getenv("RIOT_API_KEY")
 
-payload = {'game_name': 'PoggyWoggyDoggy6', 'tag_line': 'NA1'}
-r = requests.get(f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{payload["game_name"]}/{payload['tag_line']}", headers={"X-Riot-Token": api_key})
-
-print(r.content)
+def get_riot_account(game_name, tag_line):
+    load_dotenv()
+    api_key = os.getenv("RIOT_API_KEY")
+    r = requests.get(f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}", headers={"X-Riot-Token": api_key})
+    return r.json()
