34d6244fc9fdd9f0f1212c9f5a0a9acd8ac4f6df
ShaanBa <162068352+ShaanBa@users.noreply.github.com>
2025-12-17 16:56:22 -0600

    feat: added get_riot_account from riot client.py and created search user function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 import psycopg2
+from riot_client import get_riot_account
 
 app = Flask(__name__)
 
@@ -24,6 +25,12 @@ def return_dict_to_json():
         results.append({'id': row[0], 'name': row[1], 'domain': row[2], 'logo_link': row[3]})
         
     return jsonify(results)
+
+@app.route('/api/search/<game_name>/<tag_line>')
+def search_user(game_name, tag_line):
+    print(f'Searching for {game_name}')
+    return jsonify(get_riot_account(game_name, tag_line))
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
