7ca0fe0271697b0ff6f5f8fbbc83a657588fa207
ShaanBa <162068352+ShaanBa@users.noreply.github.com>
2025-12-19 13:25:04 -0600

    deleted second function get_summ_by_puuid as it is deprecated and redundant

--- riot_client.py
+++ riot_client.py
@@ -12,10 +12,6 @@ def get_riot_account(game_name, tag_line):
     r = requests.get(f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}", headers={"X-Riot-Token": api_key})
     return r.json()
 
-def get_summoner_by_puuid(puuid):
-    api_key = os.getenv("RIOT_API_KEY")
-    r = requests.get(f"https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}", headers={"X-Riot-Token": api_key})
-    return r.json()
 
 def main():
     account_details = get_riot_account('PoggyWoggyDoggy6', 'NA1')
