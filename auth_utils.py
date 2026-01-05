807a58902d7dcc2e408f04019bf07b0b59c67051
ShaanBa <162068352+ShaanBa@users.noreply.github.com>
2026-01-05 15:16:54 -0600

    feat: hash_pass function

--- auth_utils.py
+++ auth_utils.py
@@ -1,8 +1,14 @@
 import re
+import bcrypt
 def validate_email(email: str):
     if email.endswith(".edu"):
         match = re.search("@([\w.-]+\.edu)$",email)
         if match:
             return (True, match.group(1))
     return (False, None)
+
+def hash_password(plain_text):
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(plain_text.encode('utf-8'), salt)
+    return hashed.decode('utf-8')
     
