f10a530667fbc23604aef997a5edfc3a625cf7c9
ShaanBa <162068352+ShaanBa@users.noreply.github.com>
2026-01-05 15:47:52 -0600

    feat: added create_user

--- test_db.py
+++ test_db.py
@@ -1,5 +1,5 @@
 import psycopg2
-
+from psycopg2.errors import IntegrityError
 #first connect to the database 
 con = psycopg2.connect(
     host = "localhost",
@@ -50,3 +50,21 @@ def get_university_id(domain):
         return result[0]
     else:
         return None
+
+def create_user(email, password_hash, university_id):
+    con = psycopg2.connect(
+        host = "localhost",
+        database = "unileague",
+        user = "shaanbawa",
+        port = "5432",
+        password = ""
+    )
+    cur = con.cursor()
+    try:
+        query = "INSERT INTO users (user_email, password_hash, uni_id, is_verified) VALUES (%s, %s, %s, %s)"  
+        cur.execute(query, (email, password_hash, university_id, False))  
+        con.commit()
+        return True
+    except Exception as e:
+        print(f'Error creating user: {e}')
+        return False
