af4dd24eecdd5953d6334f5e243a77b2044e6bb6
ShaanBa <162068352+ShaanBa@users.noreply.github.com>
2026-01-10 18:14:20 -0600

    feat: Completed /api/register endpoint

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import psycopg2
 from riot_client import get_riot_account, get_rank_data
-from test_db import save_summoner
+from test_db import save_summoner, get_university_id, create_user
+from auth_utils import validate_email, hash_password
 
 app = Flask(__name__)
 
@@ -50,5 +51,22 @@ def search_user(game_name, tag_line):
     }
     save_summoner(full_summoner)
     return jsonify(full_summoner)
+
+@app.route('/api/register', methods=['POST'])
+def register_user():
+    data = request.get_json()
+    email = data['email']
+    password = data['password']
+    is_valid, extracted_domain = validate_email(email)
+    if not is_valid:
+        return jsonify({"error": "Not valid email"}), 400
+    uni_id = get_university_id(extracted_domain)
+    if not uni_id:
+        return jsonify({"error": "Not valid university id"}), 400
+    hashed_pass = hash_password(password)
+    user = create_user(email, hashed_pass, uni_id)
+    if not user:
+        return jsonify({"error": "Not valid User"}), 400
+    return jsonify({"message": "User created!"}), 201
 if __name__ == '__main__':
     app.run(debug=True)
