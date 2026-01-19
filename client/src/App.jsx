6530ca7922bd2b52a37e434306a30e8d3be9aa07
ShaanBa <162068352+ShaanBa@users.noreply.github.com>
2026-01-19 16:41:41 -0600

    created basic App interface linking different pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
 import { use, useState } from 'react'
-import PlayerCard from './PlayerCard'
 import Register from './Register'
 import Leaderboard from './Leaderboard'
 import SearchPage from './SearchPage'
 function App() {
+    const [currentView, setCurrentView] = useState("search")
+
+
+return (
+    <><nav>
+        <button onClick={()  => setCurrentView('search')}>Search</button>
+        <button onClick={()  => setCurrentView('register')}>Register</button>
+        <button onClick={()  => setCurrentView('leaderboard')}>Leaderboard</button>
+        
+    </nav><div>
+            {currentView === 'search' && <SearchPage />}
+            {currentView === 'register' && <Register />}
+            {currentView === 'leaderboard' && <Leaderboard />}
+        </div></>
+
+)
 }
-export default App
+export default App  
