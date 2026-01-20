ae2282df1f3793fd6a4d65c929ba27bb37bc46e6
ShaanBa <162068352+ShaanBa@users.noreply.github.com>
2026-01-20 14:13:09 -0600

    Refactored app.py to remove all sql, and renamed test_db to db_client

--- app.py
+++ app.py
@@ -1,32 +1,9 @@
 from flask import Flask, jsonify, request
-import psycopg2
 from riot_client import get_riot_account, get_rank_data
-from test_db import save_summoner, get_university_id, create_user, get_leaderboard
+from db_client import save_summoner, get_university_id, create_user, get_leaderboard
 from auth_utils import validate_email, hash_password
 
 app = Flask(__name__)
-
-@app.route("/")
-def return_dict_to_json():
-    results = []
-    con = psycopg2.connect(
-        host = "localhost",
-        database = "unileague",
-        user = "shaanbawa",
-        port = "5432",
-        password = ""
-    )
-    
-    cur = con.cursor()
-
-    cur.execute("SELECT * FROM universities")
-
-    rows = cur.fetchall()
-
-    for row in rows:
-        results.append({'id': row[0], 'name': row[1], 'domain': row[2], 'logo_link': row[3]})
-        
-    return jsonify(results)
 
 def parse_rank_data(rank_list):
     for item in rank_list:
